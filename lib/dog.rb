392ce5078f0c91517cd90621554ff99d1e4c1aba
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-03 16:03:29 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,51 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(attributes)
+    dog = self.new(attributes)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.create({id: result[0], name: result[1], breed: result[2]})
+  end
+
+end
